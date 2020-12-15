a4032eac099081460a4d1a4eaef7165a9d804158
anhnguyen300795 <anhnguyen6281@gmail.com>
2020-12-16 01:57:55 +0200

    add experiment for kd tree, 1 pods

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-ALGO=brute-force
+ALGO=kd_tree
 INDEX=1
 SERVING=1
 # export MLFLOW_EXPERIMENT_ID=$(mlflow experiments create -n $ALGO-$INDEX-index-$SERVING-serving | rev | cut -d " " -f1 | rev) && 
